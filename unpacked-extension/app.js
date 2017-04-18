a7959f601280bfecca4fa4d9761e6209d8f92620
Naveen Pai <naveen.pai@bloomreach.com>
2017-04-18 13:33:11 +0000

    Set random motivational quotes :)

--- unpacked-extension/app.js
+++ unpacked-extension/app.js
@@ -1,12 +1,23 @@
 var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var monthnames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var quotes = ['No rain, No flowers', 'Different doesn\'t mean wrong!', 'Be a voice, not an echo', 'Every moment matters', 'Think like a proton. Always positive.',
+              'Escape the ordinary!', 'Get shit done!', 'Prove them wrong!', 'Do epic shit!', '... And so the adventure begins!', 'Start somewhere.', 'Don\'t stop until you\'re proud',
+              'Impossible is just an opinion', 'If you never try, you never know', 'Calm is a superpower!', 'The story isn\'t over yet.', 'Choose happy', 'Move fast and break things!']
 
 function $(query) {
     return document.querySelectorAll(query)[0];
 }
 
 function init() {
+    setQuote();
     updateTimes();
+}
+
+function setQuote() {
+    var quoteDiv, randomQuote;
+
+    quoteDiv = document.querySelector('.quote');
+    quoteDiv.innerHTML = '"' + quotes[Math.floor(Math.random() * quotes.length)] + '"'
 }
 
 function updateTimes() {
